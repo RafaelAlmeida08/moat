232054068deae6436dbe54755f97b9e3d831aa9c
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-06-30 12:04:53 -0300

    update user form add password type and choicetype

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -14,8 +16,13 @@ class UserType extends AbstractType
         $builder
             ->add('full_name')
             ->add('username')
-            ->add('password')
-            ->add('role')
+            ->add('password', PasswordType::class)
+            ->add('role', ChoiceType::class, [
+                'choices'  => [
+                    'User' => 1,
+                    'Admin' => 2
+                ],
+            ])
         ;
     }
 
