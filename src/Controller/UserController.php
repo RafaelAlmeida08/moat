f05c78d61e279799c2ffa9dd4ee286a24fda1665
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-06-30 11:43:56 -0300

    create new user method and view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,4 +24,25 @@ class UserController extends AbstractController
             'users' => $userRepository->findAll(),
         ]);
     }
+
+    /**
+     * @Route("/new", name="app_user_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
 }
