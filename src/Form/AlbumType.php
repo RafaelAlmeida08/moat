a633b2a09e4853759478c18f1a40a4e6849670db
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-06-30 11:29:15 -0300

    update new album form add artists from api

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -6,13 +6,19 @@ use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Helpers\GetArtists;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AlbumType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $api = new GetArtists();
+        $artists = ($api->index());
         $builder
-            ->add('artist')
+            ->add('artist', ChoiceType::class, [
+                'choices'  => $artists
+            ])
             ->add('album_name')
             ->add('year')
         ;
