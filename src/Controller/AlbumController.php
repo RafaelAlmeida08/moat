49b0dfe05ddc1305b243e5fb21aa186c6503cf29
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-06-30 11:21:56 -0300

    create album controller and index view

---
+++ src/Controller/AlbumController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Album;
+use App\Form\AlbumType;
+use App\Repository\AlbumRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/album")
+ */
+class AlbumController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_album_index", methods={"GET"})
+     */
+    public function index(AlbumRepository $albumRepository): Response
+    {
+        return $this->render('album/index.html.twig', [
+            'albums' => $albumRepository->findAll(),
+        ]);
+    }
+}
