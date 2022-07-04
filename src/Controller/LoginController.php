074721b769c2d4d6bae157cd0bf307b6063b6eb0
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-07-03 22:32:56 -0300

    remove unused private variable and update indent space

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -16,32 +16,31 @@ use Symfony\Component\HttpFoundation\Session\Session;
  */
 class LoginController extends AbstractController
 {
-  private $doctrine;
-
+  
   public function __construct(ManagerRegistry $doctrine)
   {
     $this->doctrine = $doctrine;
   }
-    /**
-     * @Route("/", name="app_login_index", methods={"GET", "POST"})
-     */
-    public function index(UserRepository $userRepository, Request $request): Response
-    {
-      $form = $this->createForm(LoginType::class);
-      $form->handleRequest($request);
-      if ($form->isSubmitted() && $form->isValid()) {
-        $username = $form->get('username')->getData();
-        $password = $form->get('password')->getData();
-        $user = $userRepository->findOneByUsername($username);
-        if(!$user || $user->getPassword() !== $password) {
-          return new Response('Invalid credentials');
-        }
-        $session = new Session();
-        $session->set('user', $user);
-        return $this->redirectToRoute('app_album_index', [], Response::HTTP_SEE_OTHER);
+  /**
+   * @Route("/", name="app_login_index", methods={"GET", "POST"})
+   */
+  public function index(UserRepository $userRepository, Request $request): Response
+  {
+    $form = $this->createForm(LoginType::class);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $username = $form->get('username')->getData();
+      $password = $form->get('password')->getData();
+      $user = $userRepository->findOneByUsername($username);
+      if (!$user || $user->getPassword() !== $password) {
+        return new Response('Invalid credentials');
       }
-        return $this->renderForm('login/index.html.twig', [
-            'form' => $form
-        ]);
+      $session = new Session();
+      $session->set('user', $user);
+      return $this->redirectToRoute('app_album_index', [], Response::HTTP_SEE_OTHER);
     }
+    return $this->renderForm('login/index.html.twig', [
+      'form' => $form
+    ]);
+  }
 }
